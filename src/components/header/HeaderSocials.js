a268231cf63a7fe1549fa4c9017517db1791884d
Manish Koranga <korangamanish4@gmail.com>
2022-10-10 21:29:30 +0530

    Home Page added

---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookSquare } from "react-icons/fa";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://github.com/korangamanish4" target="-blank">
+        <FaGithub />
+      </a>
+      <a href="https://linkedin.com/in/maneesh-koranga-176653113" target="-blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://facebook.com" target="-blank">
+        <FaFacebookSquare />
+      </a>
+      <a href="https://instagram.com" target="-blank">
+        <BsInstagram />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
