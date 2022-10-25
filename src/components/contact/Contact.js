e4312c52c118edc487831d05b379e3a5d840183b
Manish Koranga <korangamanish4@gmail.com>
2022-10-25 14:44:59 +0530

    Contact Email added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./contact.css";
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+   const formData = useRef()
+
+   const sendMail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm('service_2ybbtwk', 'template_a69ps6a', formData.current, '38WLPG68JCbQUKsb5')
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+   }, function(err) {
+      console.log('FAILED...', err);
+   });
+   }
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -30,7 +44,7 @@ const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=7500843108" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={formData} onSubmit={sendMail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows={7} placeholder="Your Message" required></textarea>
